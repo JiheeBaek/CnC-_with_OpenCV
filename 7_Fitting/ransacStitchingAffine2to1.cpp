d8aed07ca9fc9af325edd9e1233714cb4f7f9367
JiheeBaek <ashely0209@naver.com>
2020-07-14 17:59:01 +0900

    codes and explanations

---
+++ 7_Fitting/ransacStitchingAffine2to1.cpp
@@ -0,0 +1,436 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/nonfree/nonfree.hpp>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include<cstdlib>
+#include<ctime>
+
+
+#define RATIO_THR 0.4
+
+using namespace std;
+using namespace cv;
+
+template <typename T>
+Mat cal_affine(int ptl_x[], int ptl_y[], int ptr_x[], int ptr_y[], int number_of_points);
+void blend_stitching(const Mat I1, const Mat I2, Mat& I_f, int diff_x, int diff_y, float alpha);
+
+double euclidDistance(Mat& vec1, Mat& vec2);
+int nearestNeighbor(Mat& vec, vector<KeyPoint>& keypoints, Mat& descriptors);
+double nearestNeighborforRB(Mat& vec, vector<KeyPoint>& keypoints, Mat& descriptors);
+void findPairs(vector<KeyPoint>& keypoints1, Mat& descriptors1,
+	vector<KeyPoint>& keypoints2, Mat& descriptors2,
+	vector<Point2f>& srcPoints, vector<Point2f>& dstPoints, bool crossCheck, bool ratio_threshold);
+
+int main() {
+
+	Mat input1 = imread("input2.jpg", CV_LOAD_IMAGE_COLOR); // right image
+	Mat input2 = imread("input1.jpg", CV_LOAD_IMAGE_COLOR); // left image
+	Mat input1_gray, input2_gray;
+
+	if (!input1.data || !input2.data)
+	{
+		std::cout << "Could not open" << std::endl;
+		return -1;
+	}
+
+	resize(input1, input1, Size(input1.cols / 2, input1.rows / 2));
+	resize(input2, input2, Size(input2.cols / 2, input2.rows / 2));
+
+	cvtColor(input1, input1_gray, CV_RGB2GRAY);
+	cvtColor(input2, input2_gray, CV_RGB2GRAY);
+
+	FeatureDetector* detector = new SiftFeatureDetector(
+		0,		// nFeatures
+		4,		// nOctaveLayers
+		0.04,	// contrastThreshold
+		10,		// edgeThreshold
+		1.6		// sigma
+		);
+
+	DescriptorExtractor* extractor = new SiftDescriptorExtractor();
+
+	// Create a image for displaying mathing keypoints
+	Size size = input2.size();
+
+	// Compute keypoints and descriptor from the source image in advance
+	vector<KeyPoint> keypoints1;
+	Mat descriptors1;
+
+	detector->detect(input1_gray, keypoints1);
+	extractor->compute(input1_gray, keypoints1, descriptors1);
+	printf("input1 : %d keypoints are found.\n", (int)keypoints1.size());
+
+	vector<KeyPoint> keypoints2;
+	Mat descriptors2;
+
+	// Detect keypoints
+	detector->detect(input2_gray, keypoints2);
+	extractor->compute(input2_gray, keypoints2, descriptors2);
+
+	printf("input2 : %zd keypoints are found.\n", keypoints2.size());
+
+	// Find nearest neighbor pairs
+	vector<Point2f> srcPoints;
+	vector<Point2f> dstPoints;
+	bool crossCheck = true;
+	bool ratio_threshold = true;
+	findPairs(keypoints2, descriptors2, keypoints1, descriptors1, srcPoints, dstPoints, crossCheck, ratio_threshold);
+	printf("%zd keypoints are matched.\n", srcPoints.size());
+
+	// ######################## RANSAC #################################
+
+	srand((unsigned int)time(NULL));
+	Mat I1, I2;
+	int k = 4;
+	int s = 5;
+	int threshold = 9;
+
+	// Read each image
+	I1 = input1.clone();     //¿ÞÂÊ
+	I2 = input2.clone();     //¿À¸¥ÂÊ
+
+	I1.convertTo(I1, CV_32FC3, 1.0 / 255);
+	I2.convertTo(I2, CV_32FC3, 1.0 / 255);
+
+	int n = srcPoints.size();
+
+	int* ptl_x = new int[n];
+	int* ptl_y = new int[n];
+	int* ptr_x = new int[n];
+	int* ptr_y = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		ptl_x[i] = dstPoints[i].y;
+		ptl_y[i] = dstPoints[i].x;
+		ptr_x[i] = srcPoints[i].y;
+		ptr_y[i] = srcPoints[i].x;
+	}
+
+
+	int* templ_x = new int[k];
+	int* templ_y = new int[k];
+	int* tempr_x = new int[k];
+	int* tempr_y = new int[k];
+	int* bestPL_x = new int[k];
+	int* bestPL_y = new int[k];
+	int* bestPR_x = new int[k];
+	int* bestPR_y = new int[k];
+
+	for (int i = 0; i < s; i++) {
+		int* inliner_x = new int[k];
+		int* inliner_y = new int[k];
+		int count = 0;
+		int maxInliner = 0;
+
+		for (int j = 0; j < k; j++) {
+			int index = rand() % n;
+			templ_x[j] = ptl_x[index];
+			templ_y[j] = ptl_y[index];
+			tempr_x[j] = ptr_x[index];
+			tempr_y[j] = ptr_y[index];
+		}
+		Mat tempA12 = cal_affine<float>(templ_x, templ_y, tempr_x, tempr_y, n);
+
+		for (int t = 0; t < k; t++) {
+			float x = tempA12.at<float>(0) * templ_x[t] + tempA12.at<float>(1) * templ_y[t] + tempA12.at<float>(2);
+			float y = tempA12.at<float>(3) * templ_x[t] + tempA12.at<float>(4) * templ_y[t] + tempA12.at<float>(5);
+			float x_ = tempr_x[t];
+			float y_ = tempr_y[t];
+			float comp_x = pow(abs(x - x_), 2);
+			float comp_y = pow(abs(y - y_), 2);
+			if (comp_x < threshold && comp_y < threshold) {
+				count++;
+				inliner_x[count] = templ_x[t];
+				inliner_y[count] = templ_y[t];
+				bestPR_x[count] = tempr_x[t];
+				bestPR_y[count] = tempr_y[t];
+
+			}
+		}
+		tempA12.release();
+		if (count > maxInliner) {
+			maxInliner = count;
+			bestPL_x = inliner_x;
+			bestPL_y = inliner_y;
+		}
+		delete inliner_x;
+		delete inliner_y;
+	}
+	delete ptl_x;
+	delete ptl_y;
+	delete ptr_x;
+	delete ptr_y;
+
+	delete templ_x;
+	delete templ_y;
+	delete tempr_x;
+	delete tempr_y;
+
+	input1.release();
+	input2.release();
+	input1_gray.release();
+	input2_gray.release();
+	descriptors1.release();
+	descriptors2.release();
+
+	srcPoints.shrink_to_fit();
+	dstPoints.shrink_to_fit();
+
+	// ############################################################
+	// height(row), width(col) of each image
+	const float I1_row = I1.rows;
+	const float I1_col = I1.cols;
+	const float I2_row = I2.rows;
+	const float I2_col = I2.cols;
+
+	// calculate affine Matrix A12, A21
+	Mat A12 = cal_affine<float>(bestPL_x, bestPL_y, bestPR_x, bestPR_y, n);
+	Mat A21 = cal_affine<float>(bestPR_x, bestPR_y, bestPL_x, bestPL_y, n);
+
+	delete bestPL_x;
+	delete bestPL_y;
+	delete bestPR_x;
+	delete bestPR_y;
+
+	// compute corners (p1, p2, p3, p4)
+	Point2f p1(A21.at<float>(0) * 0 + A21.at<float>(1) * 0 + A21.at<float>(2),
+		A21.at<float>(3) * 0 + A21.at<float>(4) * 0 + A21.at<float>(5));
+
+	Point2f p2(A21.at<float>(0) * 0 + A21.at<float>(1) * I2_col + A21.at<float>(2),
+		A21.at<float>(3) * 0 + A21.at<float>(4) * I2_col + A21.at<float>(5));
+
+	Point2f p3(A21.at<float>(0) * I2_row + A21.at<float>(1) * 0 + A21.at<float>(2),
+		A21.at<float>(3) * I2_row + A21.at<float>(4) * 0 + A21.at<float>(5));
+
+	Point2f p4(A21.at<float>(0) * I2_row + A21.at<float>(1) * I2_col + A21.at<float>(2),
+		A21.at<float>(3) * I2_row + A21.at<float>(4) * I2_col + A21.at<float>(5));
+
+	// for inverse warping
+	Point2f p1_(A12.at<float>(0) * 0 + A12.at<float>(1) * 0 + A12.at<float>(2),
+		A12.at<float>(3) * 0 + A12.at<float>(4) * 0 + A12.at<float>(5));
+
+	Point2f p2_(A12.at<float>(0) * 0 + A12.at<float>(1) * I1_col + A12.at<float>(2),
+		A12.at<float>(3) * 0 + A12.at<float>(4) * I1_col + A12.at<float>(5));
+
+	Point2f p3_(A12.at<float>(0) * I1_row + A12.at<float>(1) * 0 + A12.at<float>(2),
+		A12.at<float>(3) * I1_row + A12.at<float>(4) * 0 + A12.at<float>(5));
+
+	Point2f p4_(A12.at<float>(0) * I1_row + A12.at<float>(1) * I1_col + A12.at<float>(2),
+		A12.at<float>(3) * I1_row + A12.at<float>(4) * I1_col + A12.at<float>(5));
+
+	// compute boundary for merged image(I_f)
+	int bound_u = (int)round(min(0.0f, min(p1.x, p2.x)));
+	int bound_b = (int)round(std::max(I1_row, std::max(p3.x, p4.x)));
+	int bound_l = (int)round(min(0.0f, min(p1.y, p3.y)));
+	int bound_r = (int)round(std::max(I1_col, std::max(p2.y, p4.y)));
+
+	// compute boundary for inverse warping
+	int bound_u_ = (int)round(min(0.0f, min(p1_.x, p2_.x)));
+	int bound_b_ = (int)round(std::max(I2_row, std::max(p3_.x, p4_.x)));
+	int bound_l_ = (int)round(min(0.0f, min(p1_.y, p3_.y)));
+	int bound_r_ = (int)round(std::max(I2_col, std::max(p2_.y, p4_.y)));
+
+	int diff_x = abs(bound_u);
+	int diff_y = abs(bound_l);
+
+	int diff_x_ = abs(bound_u_);
+	int diff_y_ = abs(bound_l_);
+	printf("done ");
+	// initialize merged image
+	Mat I_f(bound_b - bound_u + 1, bound_r - bound_l + 1, CV_32FC3, Scalar(0));
+	printf("done ");
+	// inverse warping with bilinear interplolation
+	for (int i = -diff_x_; i < I_f.rows - diff_x_; i++) {
+		for (int j = -diff_y_; j < I_f.cols - diff_y_; j++) {
+			float x = A12.at<float>(0) * i + A12.at<float>(1) * j + A12.at<float>(2) + diff_x_;
+			float y = A12.at<float>(3) * i + A12.at<float>(4) * j + A12.at<float>(5) + diff_y_;
+
+			float y1 = floor(y);
+			float y2 = ceil(y);
+			float x1 = floor(x);
+			float x2 = ceil(x);
+
+			float mu = y - y1;
+			float lambda = x - x1;
+			if (x1 >= 0 && x2 < I2_row && y1 >= 0 && y2 < I2_col)
+				I_f.at<cv::Vec3f>(i + diff_x_, j + diff_y_) = lambda * I2.at<cv::Vec3f>(x2, y) + (1 - lambda) * I2.at<cv::Vec3f>(x1, y);
+
+		}
+	}
+
+	// image stitching with blend
+	blend_stitching(I1, I2, I_f, diff_x, diff_y, 0.5);
+
+	namedWindow("Left Image");
+	imshow("Left Image", I1);
+
+	namedWindow("Right Image");
+	imshow("Right Image", I2);
+
+	namedWindow("result");
+	imshow("result", I_f);
+
+	waitKey(0);
+
+	return 0;
+}
+
+/**
+* Calculate euclid distance
+*/
+double euclidDistance(Mat& vec1, Mat& vec2) {
+	double sum = 0.0;
+	int dim = vec1.cols;
+	for (int i = 0; i < dim; i++) {
+		sum += (vec1.at<float>(0, i) - vec2.at<float>(0, i)) * (vec1.at<float>(0, i) - vec2.at<float>(0, i));
+	}
+
+	return sqrt(sum);
+}
+
+/**
+* Find the index of nearest neighbor point from keypoints.
+*/
+int nearestNeighbor(Mat& vec, vector<KeyPoint>& keypoints, Mat& descriptors) {
+	int neighbor = -1;
+	double minDist = 1e6;
+
+	for (int i = 0; i < descriptors.rows; i++) {
+		Mat v = descriptors.row(i);		// each row of descriptor
+
+		//
+		//	Fill the code
+		//
+		double dist = euclidDistance(vec, v);
+		if (dist < minDist) {
+			minDist = dist;
+			neighbor = i;
+		}
+	}
+
+	return neighbor;
+}
+
+double nearestNeighborforRB(Mat& vec, vector<KeyPoint>& keypoints, Mat& descriptors) {
+	int neighbor = -1;
+	double first = 1e6;
+	double second = 1e6;
+
+	for (int i = 0; i < descriptors.rows; i++) {
+		Mat v = descriptors.row(i);		// each row of descriptor
+
+		//
+		//	Fill the code
+		//
+		double dist = euclidDistance(vec, v);
+		if (dist < first) {
+			second = first;
+			first = dist;
+		}
+		else if ((second > dist) && (dist > first)) {
+			second = dist;
+		}
+	}
+
+	double ratio = first / second;
+	return ratio;
+}
+
+/**
+* Find pairs of points with the smallest distace between them
+*/
+void findPairs(vector<KeyPoint>& keypoints1, Mat& descriptors1,
+	vector<KeyPoint>& keypoints2, Mat& descriptors2,
+	vector<Point2f>& srcPoints, vector<Point2f>& dstPoints, bool crossCheck, bool ratio_threshold) {
+	for (int i = 0; i < descriptors1.rows; i++) {
+		KeyPoint pt1 = keypoints1[i];
+		Mat desc1 = descriptors1.row(i);
+
+		int nn = nearestNeighbor(desc1, keypoints2, descriptors2);
+
+		// Refine matching points using ratio_based thresholding
+		if (ratio_threshold) {
+			//
+			//	Fill the code
+			//
+			double ratio = nearestNeighborforRB(desc1, keypoints2, descriptors2);
+			if (ratio < RATIO_THR) {}
+			else
+				continue;
+
+		}
+
+		// Refine matching points using cross-checking
+		if (crossCheck) {
+			//
+			//	Fill the code
+			//
+			Mat desc2 = descriptors2.row(nn);
+			int l = nearestNeighbor(desc2, keypoints1, descriptors1);
+			if (i == l) {}
+			else
+				continue;
+		}
+
+		KeyPoint pt2 = keypoints2[nn];
+		srcPoints.push_back(pt1.pt);
+		dstPoints.push_back(pt2.pt);
+	}
+}
+
+template <typename T>
+Mat cal_affine(int ptl_x[], int ptl_y[], int ptr_x[], int ptr_y[], int number_of_points) {
+
+	Mat M(2 * number_of_points, 6, CV_32F, Scalar(0));
+	Mat b(2 * number_of_points, 1, CV_32F);
+
+	Mat M_trans, temp, affineM;
+
+	// initialize matrix
+	for (int i = 0; i < number_of_points; i++) {
+		M.at<float>(2 * i, 0) = ptl_x[i];
+		M.at<float>(2 * i, 1) = ptl_y[i];
+		M.at<float>(2 * i, 2) = 1;
+		b.at<float>(2 * i) = ptr_x[i];
+
+		M.at<float>(2 * i + 1, 3) = ptl_x[i];
+		M.at<float>(2 * i + 1, 4) = ptl_y[i];
+		M.at<float>(2 * i + 1, 5) = 1;
+		b.at<float>(2 * i + 1) = ptr_y[i];
+	}
+	transpose(M, M_trans);
+	temp = (M_trans * M).inv() * M_trans;
+	affineM = temp * b;
+
+	return affineM;
+}
+
+void blend_stitching(const Mat I1, const Mat I2, Mat& I_f, int diff_x, int diff_y, float alpha) {
+
+	int bound_x = I1.rows + diff_x;
+	int bound_y = I1.cols + diff_y;
+
+	int col = I_f.cols;
+	int row = I_f.rows;
+
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			// for check validation of I1 & I2
+			bool cond1 = (i < bound_x&& i > diff_x) && (j < bound_y&& j > diff_y) ? true : false;
+			bool cond2 = I_f.at<Vec3f>(i, j) != Vec3f(0, 0, 0) ? true : false;
+
+			// I2 is already in I_f by inverse warping
+			// So, It is not necessary to check that only I2 is valid
+			// if both are valid
+			if (cond1 && cond2) {
+				I_f.at<Vec3f>(i, j) = alpha * I1.at<Vec3f>(i - diff_x, j - diff_y) + (1 - alpha) * I_f.at<Vec3f>(i, j);
+			}
+			// only I1 is valid
+			else if (cond1) {
+				I_f.at<Vec3f>(i, j) = I1.at<Vec3f>(i - diff_x, j - diff_y);
+			}
+		}
+	}
+}
